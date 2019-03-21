506f931caacfa7109b7c0f948fe70eb3414821d4
Francesco Pio Scognamiglio <36161521+francescopioscognamiglio@users.noreply.github.com>
2019-03-21 09:37:05 +0100

    StampaNome Project
    Print each input names between two rows of dashes

---
+++ StampaNome/src/stampanome/StampaNome.java
@@ -0,0 +1,28 @@
+package stampanome;
+/**
+ * Scrivere una classe StampaNome che,
+ * dato in input uno o più nomi,
+ * li stampi tra due righe di trattini.
+ * Ad es.:
+ * +---------+
+ * Roberto
+ * +---------+
+ * @author francescopioscognamiglio
+ *
+ */
+public class StampaNome
+{
+	
+	public static void main(String[] args)
+	{
+		final String trattini = "+---------+";
+		
+		for (String nome : args)
+		{
+			System.out.println(trattini);
+			System.out.println(nome);
+			System.out.println(trattini);
+		}
+	}
+	
+}
