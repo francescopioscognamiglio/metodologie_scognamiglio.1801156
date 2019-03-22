08d0b16ab1a640dfbfdc9db6e7b31fd142f831bb
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2019-03-22 13:31:25 +0100

    AnnoBisestile Project use default package

---
+++ AnnoBisestile/src/AnnoBisestile.java
@@ -0,0 +1,29 @@
+
+/**
+ * Verificare se un anno è bisestile.
+ * @author francescopioscognamiglio
+ *
+ */
+public class AnnoBisestile
+{
+	
+	/**
+	 * metodo che restituisce true se un anno è bisestile; false altrimenti
+	 * @return true se un anno è bisestile; false altrimenti
+	 */
+	public static boolean isAnnoBisestile(String annoInput)
+	{
+		int anno = Integer.parseInt(annoInput);
+		return (anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0);
+	}
+	
+	public static void main(String[] args)
+	{
+		for (String arg : args)
+		{
+			boolean bBisestile = isAnnoBisestile(arg);
+			System.out.println("L'anno " + arg + " e' bisestile? " + bBisestile);
+		}
+	}
+	
+}
