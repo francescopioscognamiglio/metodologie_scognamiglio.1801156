8beec1f56a47f0508a77171e2c000e0a24db6177
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2019-03-23 01:31:19 +0100

    Improve javadoc

--- Variabili/src/Variabili.java
+++ Variabili/src/Variabili.java
@@ -1,15 +1,15 @@
 /**
- * Scrivere una classe Variabili che, all’interno del metodo main,
- * dichiari una variabile intera i,
- * una variabile di tipo stringa s e una variabile double d.
+ * Esercizi per cominciare
  * 
+ * Scrivere una classe Variabili che, all’interno del metodo main,
+ * dichiari una variabile intera i, una variabile di tipo stringa s
+ * e una variabile double d.
  * Quindi vengono svolte le seguenti tre operazioni:
- * - La stringa viene inizializzata al valore
- * 		del primo argomento fornito in input al main
+ * - La stringa viene inizializzata al valore del primo argomento fornito in input al main
  * - All’intero viene assegnato il valore intero della stringa
- * - Al double viene assegnata la metà del valore di i
- * 		(ad es. se i è pari a 3, d sarà pari a 1.5)
+ * - Al double viene assegnata la metà del valore di i (ad es. se i è pari a 3, d sarà pari a 1.5)
  * - I valori di s, i e d vengono stampati a video
+ * 
  * @author francescopioscognamiglio
  * 
  */
