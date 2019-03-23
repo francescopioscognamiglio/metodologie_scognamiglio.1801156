fe9e2d6b41b71ec9275dd16e5485197e1ad4eef4
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2019-03-23 01:28:47 +0100

    use moltiplica method to return multiplication

--- Moltiplica/src/Moltiplica.java
+++ Moltiplica/src/Moltiplica.java
@@ -1,19 +1,26 @@
 
 /**
- * Scrivere una classe Moltiplica che,
- * dati in input 2 numeri interi,
- * ne restituisca a video il prodotto.
+ * Esercizi per cominciare
+ * 
+ * Scrivere una classe Moltiplica che, dati in input 2 numeri interi,
+ * ne restituisca a video il prodotto
+ * 
  * @author francescopioscognamiglio
  *
  */
 public class Moltiplica
 {
 	
+	public static int moltiplica(int num1, int num2)
+	{
+		return num1*num2;
+	}
+	
 	public static void main(String[] args)
 	{
 		int num1 = Integer.parseInt(args[0]);
 		int num2 = Integer.parseInt(args[1]);
-		System.out.println("Il prodotto tra i numeri " + num1 + " e " + num2 + " e': " + num1*num2);
+		System.out.println("Il prodotto tra i numeri " + num1 + " e " + num2 + " e': " + moltiplica(num1, num2));
 	}
 	
 }
