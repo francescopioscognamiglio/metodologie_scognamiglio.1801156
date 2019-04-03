a4c66b88cd1fafd2a05f775d6e60e9dc9c005f01
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2019-04-03 09:52:44 +0200

    Clean code

--- ContoBancario/src/ContoBancarioTest.java
+++ ContoBancario/src/ContoBancarioTest.java
@@ -5,12 +5,12 @@ public class ContoBancarioTest
 	public static void main(String[] args) throws Exception
 	{
 		ContoBancario contoBancario = new ContoBancario("10112XADQ12", 200.0);
+		
 		contoBancario.eseguiOperazione(new PrelevaDenaro(contoBancario, 100.0));
 		contoBancario.eseguiOperazione(new SvuotaConto(contoBancario));
 		contoBancario.eseguiOperazione(new VersaDenaro(contoBancario, 200.0));
 		contoBancario.eseguiOperazione(new SituazioneConto(contoBancario));
 		contoBancario.eseguiOperazione(new Bonifico(contoBancario, 40.0, new ContoBancario("XXXX")));
-		
 		
 		System.out.println(contoBancario.getStorico());
 	}
